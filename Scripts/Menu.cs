a902a65edce4e2f17f4c31d5d9060a95830599a2
SirDante830 <dantec830@gmail.com>
2024-02-19 17:20:13 -0500

    Testing Project Settings Push

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -7,6 +7,7 @@ public class Menu : MonoBehaviour
 {
 
     public bool isStart;
+    public bool isStartAgain;
     public bool isQuit;
 
     void OnMouseUp()
@@ -15,6 +16,11 @@ public class Menu : MonoBehaviour
         {
             Destroy(GameObject.Find("Music"));
             SceneManager.LoadScene("Level");
+        }
+        if(isStartAgain)
+        {
+            Destroy(GameObject.Find("Music"));
+            SceneManager.LoadScene("MainMenu");
         }
         if (isQuit)
         {
